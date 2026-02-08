4e0495c3f381aab5e7e2fea756faf1daa469eee6
oto20 <alexan4039@gmail.com>
2026-02-08 19:54:11 +0900

    変更

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// セクション（+）
 document.querySelectorAll('.menu-item:not(.static) > .menu-title')
   .forEach(btn => {
     btn.addEventListener('click', () => {
@@ -6,22 +5,6 @@ document.querySelectorAll('.menu-item:not(.static) > .menu-title')
 
       // 他を閉じる
       document.querySelectorAll('.menu-item.open')
-        .forEach(i => {
-          if (i !== item) i.classList.remove('open');
-        });
-
-      item.classList.toggle('open');
-    });
-  });
-
-// ● item（text内など）
-document.querySelectorAll('.item-title')
-  .forEach(btn => {
-    btn.addEventListener('click', () => {
-      const item = btn.parentElement;
-
-      // 同じ submenu 内で1つだけ開く
-      item.parentElement.querySelectorAll('.item.open')
         .forEach(i => {
           if (i !== item) i.classList.remove('open');
         });
